55bc1c239bd2fd8aca5892a5fbcd269215f655bd
rzezzy1 <rzezzy1@gmail.com>
2026-06-19 00:30:24 -0400

    Defined the Item class

--- engine/models.py
+++ engine/models.py
@@ -1,7 +1,28 @@
-import enum
-from dataclasses import dataclass, field
+from dataclasses import Field, dataclass
+from enum import Enum
+
+
+class Form(Enum):
+    RF_SOLID = "RF_SOLID"
+    RF_LIQUID = "RF_LIQUID"
+    RF_GAS = "RF_GAS"
+
+
+class StackSize(Enum):
+    SS_ONE = 1
+    SS_SMALL = 50
+    SS_MEDIUM = 100
+    SS_BIG = 200
+    SS_HUGE = 500
+    SS_FLUID = 50000
 
 
 @dataclass
 class Item:
-    mStackSize: enum
+    item_name: str
+    display_name: str
+    stack_size: StackSize
+    can_be_discarded: bool = True
+    resource_sink_points: int = 0
+    energy_value: float = 0.0
+    form: Form = Form.RF_SOLID
