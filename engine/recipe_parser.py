366ecdb4e3a30e115608bc9d4dbd8648da9461a3
rzezzy1 <rzezzy1@gmail.com>
2026-06-25 23:34:20 -0400

    Wrote first part of recipe parser and left a comment for myself to maybe come back later and clean the two loops into one

--- engine/recipe_parser.py
+++ engine/recipe_parser.py
@@ -5,3 +5,16 @@ docs_path = "scripts/en-US.json"
 
 with open(docs_path, mode="r", encoding="utf-16") as read_file:
     docs_raw = json.load(read_file)
+
+docs_unwrap_intermediate: dict[str, dict] = {}
+docs_unwrap: dict[str, dict] = {}
+
+# # TODO: tighten this up into a single loop
+for category in docs_raw:
+    docs_unwrap_intermediate[category["NativeClass"].split(".")[2].rstrip("'")] = (
+        category["Classes"]
+    )
+    docs_unwrap[category["NativeClass"].split(".")[2].rstrip("'")] = {}
+for native_class, class_list in docs_unwrap_intermediate.items():
+    for class_dict in docs_unwrap_intermediate[native_class]:
+        docs_unwrap[native_class][class_dict["ClassName"]] = class_dict
